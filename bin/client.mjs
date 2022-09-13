2dafe7f0b478a9813c4150192a9cf6636b2068f2
Finn Lancaster <flancast90@gmail.com>
2022-09-13 09:10:30 +0000

    Use parser version flag to download package into node_modules for that specific release

--- bin/client.mjs
+++ bin/client.mjs
@@ -1,6 +1,5 @@
 import { Colors } from './parser.mjs';
 import axios from 'axios';
-import { writeFile } from 'fs';
 import npm from 'npm-programmatic';
 import process from 'process';
 import path from 'path';
@@ -9,7 +8,7 @@ import fs from 'fs';
 export async function update(url="https://flancast90.github.io/wpm/wasp.json") {
  try {
   const latest = await axios.get(url);
-  if (latest.status === 200) return writeToErrors(latest.data)
+  if (latest.status === 200) return writeToErrors(JSON.stringify(latest.data))
   else return Colors.warn("Error: Could not update known packages file.");
  } catch(e) {
   return Colors.warn("Error: Could not update known packages file.");
@@ -17,39 +16,41 @@ export async function update(url="https://flancast90.github.io/wpm/wasp.json") {
 }
 
 function writeToErrors(data) {
- writeFile('wasp.json', data, function (err) {
+ fs.writeFileSync(path.join(process.cwd(), 'wasp.json'), data, function (err) {
   if (err) return Colors.error(err);
   return Colors.success("Success: Updated known packages file.")
  });
 }
 
-export function install(name) {
+export function install(name, version) {
+ const v = (version) ? `@${version}` : '';
  const pkgs = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'wasp.json')));
  
  if (pkgs.hasOwnProperty(name)) return Colors.error(pkgs[name].error);
- npm.install([name], {
+ npm.install([`${name}${v}`], {
   cwd: process.cwd(),
   save: true
  }).then(function() {
-  return Colors.success("Success! Package '${name}' installed in /node_modules");
+  return Colors.success(`Success! Package '${name}${v}' installed in /node_modules`);
  }).catch(function() {
-  return Colors.error("Error: unable to install package '${name}'");
+  return Colors.error(`Error: unable to install package '${name}${v}'`);
  });
 }
 
-export function uninstall(name) {
+export function uninstall(name, version) {
+ const v = (version) ? `@${version}` : '';
  npm.list(process.cwd()).then(function(res) {
-  if (res.includes(name)) {
-    npm.uninstall([name], {
+  if (res.includes(`name${v}`)) {
+    npm.uninstall([`name${v}`], {
      cwd: process.cwd(),
      save: true
     }).then(function() {
-     Colors.success("Package '${name} uninstalled successfully.");
+     Colors.success(`Package '${name}${v}' uninstalled successfully.`);
     }).catch(function() {
-     Colors.error("Unknown error uninstalling '${name}'.");
+     Colors.error(`Unknown error uninstalling '${name}${v}'.`);
     });
   } else {
-    return Colors.error("Error: package '${name}' not installed.");
+    return Colors.error(`Error: package '${name}${v}' not installed.`);
   }
  });
 }
